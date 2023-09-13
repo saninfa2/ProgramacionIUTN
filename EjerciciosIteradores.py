d7c58d47cefad5d2a7a044ebe295d222d299e70d
saninfa2 <106618519+saninfa2@users.noreply.github.com>
2023-09-13 16:51:52 -0300

    fix: información al usuario sobre la condicion de salida, punto 2

--- EjerciciosIteradores.py
+++ EjerciciosIteradores.py
@@ -1,5 +1,13 @@
 #Ejercicio en clase - Iteradores for y while
 #Ejercicio 1 - for
+#Uno de los equipos decide utilizar un método antiguo de encriptación llamado “la cifra del césar”, 
+# que consiste en correr cada letra del mensaje –considerando la posición de cada una en el 
+# alfabeto– una determinada cantidad de lugares. Ejemplo: si el corrimiento es de 2 lugares, la 
+#palabra “ATAQUE” se transforma en “CVCSWG”. Cada día, el “jefe” del equipo debe enviar un mensaje
+#a cada uno de sus oficiales. Escribir un programa que permita encriptar los 5 mensajes. 
+#El corrimiento (cantidad de lugares que se correrán las letras) será dado por el usuario antes de 
+#comenzar a encriptar. Los 5 mensajes usarán el mismo corrimiento.
+
 corrimiento = int(input("Ingrese la cantidad de valores que se correra su frase "))
 abece="ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
@@ -16,12 +24,16 @@ for i in range(5):
     print(frase_enc)
 
 #Ejercicio 2 - while
+#Crear un programa que solicite el ingreso de números enteros positivos, 
+# hasta que el usuario ingrese el 0. Por cada número, informar cuántos dígitos pares y 
+# cuántos impares tiene. Al finalizar, informar la cantidad de dígitos pares y de 
+# dígitos impares leídos en total.
 print("Números positivos")
 numero=1
 total_par=0
 total_impar=0
 while numero != "0":
-    numero = input("Ingrese una cifra para desglozarla ")
+    numero = input("Ingrese una numeros enteros positivos o 0 (cero) para salir: ")
     par=0
     impar=0
     for i in numero:
