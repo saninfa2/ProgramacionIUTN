d80e8faf099a59f6b1725b29027cc0ea6b249153
saninfa2 <106618519+saninfa2@users.noreply.github.com>
2023-10-26 19:57:09 -0300

    fix: comentarios y arreglo de booleanos

--- VariablesDimensionadas/tpClase_varDimen.py
+++ VariablesDimensionadas/tpClase_varDimen.py
@@ -1,11 +1,17 @@
 import funcionesCopy
+#Tuve que copiar el archivo de funciones porque no me encontraba el paquete pero 
+#eliminé las demás funciones para que no fuese un archivo tan pesao
+#Por eso el nombre
+
 #--------------------------VARIABLES DIMENSIONADAS---------------------------------
 #-------------------------------EJERCICIO 1---------------------------------------
+#Inicialización de variables
 passengers = []
 cities = []
-option_valid = False
+option_valid = True
 
-while option_valid != True:
+#Mientras la condición sea distinta de falso o se ingrese la opcion de salir, se presenta el menu 
+while option_valid != False:
     print("\nMenu: 1. Agregar pasajero  2. Agregar ciudad  3. Buscar ciudad por DNI  ")
     print("4. Buscar cuántos pasajeros por ciudad  5. Buscar país por DNI")
     print("6. Buscar cuántos pasajeros por país  7. Salir")
@@ -14,22 +20,16 @@ while option_valid != True:
 
     if choice == "1":
         funcionesCopy.add_passenger(passengers)
-        option_valid = True
     elif choice == "2":
-        funcionesCopy.add_city(cities)
-        option_valid = True
+        funcionesCopy.add_city(cities) 
     elif choice == "3":
         funcionesCopy.city_by_dni(passengers, cities)
-        option_valid = True
     elif choice == "4":
         funcionesCopy.passengers_by_city(passengers, cities)
-        option_valid = True
     elif choice == "5":
         funcionesCopy.country_by_dni(passengers, cities)
-        option_valid = True
     elif choice == "6":
         funcionesCopy.passengers_by_country(passengers, cities)
-        option_valid = True
     elif choice == "7":
         print("Saliendo del programa.")
         break
@@ -46,22 +46,24 @@ compras = [
     ('Luis Martinez', 15, 987.0, 'Calle 3 - 123')
 ]
 
+#Llamada a la función 
 domicilios_factura = funcionesCopy.get_invoice_addresses(compras)
+
+#Mostramos el resultado por pantalla
 print("Domicilios a los que se debe enviar una factura:")
 for domicilio in domicilios_factura:
     print(domicilio)
 
 #-------------------------------- EJERCICIO 3 -------------------------------------------
-# Datos iniciales
+#Inicialización de varibales
 members = {
     1: {"name": "Amanda Núñez", "join_date": "17/03/2009", "dues_up_to_date": True},
     2: {"name": "Bárbara Molina", "join_date": "17/03/2009", "dues_up_to_date": True},
     3: {"name": "Lautaro Campos", "join_date": "17/03/2009", "dues_up_to_date": True}
 }
+option_valid = True
 
-option_valid = False
-
-while option_valid != True:
+while option_valid != False:
     print("\nMenú:  1. Informar cantidad de socios  2. Registrar pago de cuotas  3. Modificar fecha de ingreso")
     print("4. Dar de baja a un socio  5. Imprimir listado de socios  6. Salir")
 
@@ -69,22 +71,17 @@ while option_valid != True:
 
     if option == "1":
         funcionesCopy.show_number_members(members)
-        option_valid = True
     elif option == "2":
         member_number = int(input("Ingrese el número de socio que ha pagado todas las cuotas: "))
         funcionesCopy.record_fee_payment(member_number, members)
-        option_valid = True
     elif option == "3":
         funcionesCopy.modify_entry_date(members)
-        option_valid = True
         print("Fechas de ingreso modificadas.")
     elif option == "4":
         name_surname = input("Ingrese el nombre y apellido del socio a dar de baja: ")
         funcionesCopy.unsubscribe(name_surname, members)
-        option_valid = True
     elif option == "5":
         funcionesCopy.print_members_list(members)
-        option_valid = True
     elif option == "6":
         print("Saliendo del programa.")
         break
