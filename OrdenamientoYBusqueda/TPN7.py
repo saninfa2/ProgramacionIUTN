cc50bb46ecd068a736a21af5ec0e01576b1610b3
saninfa2 <106618519+saninfa2@users.noreply.github.com>
2023-10-29 10:12:32 -0300

    tp n7 terminado, me había olvidado de guardar

--- OrdenamientoYBusqueda/TPN7.py
+++ OrdenamientoYBusqueda/TPN7.py
@@ -1,21 +1,16 @@
 from medOrdenamiento import *
-#Escribe un programa que tome una lista de números como entrada y la ordene en orden ascendente 
-#utilizando el método de ordenamiento de burbuja.
+
 #Ejercicio 1
 list_to_bubble = [3,4,2,6,-1]
 bubble_sort(list_to_bubble)
 showList(list_to_bubble)
 
-#Crea un programa que tome una lista de palabras como entrada y las ordene alfabéticamente 
-#en orden ascendente utilizando el método de ordenamiento de selección.
 #Ejercicio 2
 list_to_select = ["hola", "manzana", "naranja", "banana"]
 select_sort(list_to_select)
 showList(list_to_select)
 
-#Crea una lista de diccionarios, donde cada diccionario contiene información sobre un libro 
-#(título, autor, año de publicación, etc.). Luego, escribe un programa que ordene la lista de 
-#libros en función de un campo específico, como el año de publicación o el autor.
+#Ejercicio 3
 list_of_book = [{"Titulo": "Rayuela", "Autor": "Cortazar", "anioPublicacion" : 1963},
                 {"Titulo": "Cien años de soledad", "Autor": "Marquez", "anioPublicacion" : 1967},
                 {"Titulo": "Poemas", "Autor": "Neruda", "anioPublicacion" : 1924},
@@ -27,8 +22,6 @@ for book in list_of_book:
     print(f"Título: {book['Titulo']}, Autor: {book['Autor']}, Año: {book['anioPublicacion']}")
     
 #Ejercicio 4
-#Escribe un programa que tome una lista de cadenas como entrada y las ordene en orden ascendente 
-#según su longitud. Puedes utilizar el método de ordenamiento de inserción
 list_to_insert_str = ["hola", "manzana", "naranja", "banana"]
 insert_sort_string(list_to_insert_str)
 showList(list_to_select)
@@ -40,8 +33,6 @@ showList(list_to_bubble)
 
 
 #Ejercicio 6
-#Escribe un programa que tome una lista de números enteros y ordene la lista utilizando el 
-#algoritmo de ordenamiento por conteo.
 list_to_count = [3,4,2,6]
 countsort(list_to_count,6)
 showList(list_to_count)
